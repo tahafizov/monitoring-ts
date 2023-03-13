5026ce6724db1928723f13ae608d024fdd3db9d0
tahafizov <tahafizov@gmail.com>
2023-03-13 21:54:09 +0500

    Добавление модуля kafkaProducer

--- src/main/java/ru/otus/kafkaproducer/service/impl/KafkaSendServiceImpl.java
+++ src/main/java/ru/otus/kafkaproducer/service/impl/KafkaSendServiceImpl.java
@@ -20,21 +20,17 @@ public class KafkaSendServiceImpl implements KafkaSendService {
 
     private static final String TRACK_NUMBER = "9876543212";
 
+    private static final Point testPoint = Point.builder()
+            .id(8452L)
+            .time(OffsetDateTime.now())
+            .trackNumber(TRACK_NUMBER)
+            .latitude(43.603760D)
+            .longitude(39.716172)
+            .build();
+
     @Override
     public void send() {
-        Point testPoint = getPoint();
         log.info("sending payload='{}'", testPoint);
-        var future = kafkaTemplate.send(topicName, testPoint);
-        future.whenCompleteAsync((result, throwable) -> {
-            if (throwable != null) {
-                log.info("Unable to send message='{}' due to : {}", testPoint, throwable.getMessage());
-            } else {
-                log.info("Sent message='{}' successfully with offset={}", testPoint, result.getRecordMetadata().offset());
-            }
-        });
-    }
-
-    private Point getPoint() {
-        return new Point(8452L, OffsetDateTime.now(), TRACK_NUMBER,43.603760D, 39.716172);
+        kafkaTemplate.send(topicName, testPoint);
     }
 }
